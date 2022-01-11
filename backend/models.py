23a66cc96dbd41ce239486b3a2d5833ae9411b19
sonukumar-a11 <sharmasonu04359491@gmail.com>
2022-01-12 00:24:17 +0530

    Creating Modles in App

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
+import uuid
 from django.db import models
 from django.core.validators import MinLengthValidator
-import uuid
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -26,9 +27,8 @@ class PatientDetails(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICE, default=GENDER_MALE)
     address = models.CharField(max_length=255)
     dob = models.DateTimeField()
-    phone_number = models.CharField(max_length=10, null=False, blank=False, unique=False)
+    phone_number = PhoneNumberField()
     doctor = models.ManyToManyField(DoctorDetails, blank=True)
-
 
 
 class Allergy(models.Model):
@@ -53,7 +53,7 @@ class Allergy(models.Model):
     verification_status = models.CharField(max_length=40, choices=VERIFICATION_STATUS_CHOICE)
     criticality = models.CharField(max_length=40, choices=CRITICALITY_CHOICE)
     type = models.CharField(max_length=40, choices=TYPE_CHOICE)
-    comment = models.CharField(max_length=255, null=True, blank=True)
+    comment = models.TextField(blank=True)
 
 
 class VitalDetails(models.Model):
@@ -64,3 +64,53 @@ class VitalDetails(models.Model):
     pulse = models.FloatField()
     date_added = models.DateTimeField(auto_now_add=True)
     temperature = models.FloatField()
+
+
+class Medication(models.Model):
+    patient = models.OneToOneField(PatientDetails, on_delete=models.PROTECT, related_name='patient_med')
+    doctor = models.OneToOneField(DoctorDetails, on_delete=models.SET_NULL, null=True)
+    medication_name = models.CharField(max_length=40),
+    medication_manufacturer = models.CharField(max_length=40)
+    expire = models.DateTimeField()
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Dosage(models.Model):
+    DOSETIMECHOICE = [
+        ("Per Day", "1/d"), ("Per Half Day", "1/h"), ("Per Quater Hour", "1/Q")
+    ]
+
+    medication = models.OneToOneField(Medication, on_delete=models.CASCADE, related_name='med')
+    dose_amount = models.PositiveSmallIntegerField()
+    dose_timing = models.CharField(max_length=20, choices=DOSETIMECHOICE)
+    dose_description = models.TextField()
+class ProblemDetails(models.Model):
+    SEVERITIES = [("1", "Mild"), ("2", "Moderate"), ("3", "Severe")]
+    STATUS = [("A", "Active"), ("R", "Resolved")]
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    problem_name = models.TextField()
+    severity = models.CharField(max_length=10, choices=SEVERITIES, default="Mild")
+    status = models.CharField(max_length=15, choices=STATUS, default="Active")
+    start_date = models.DateField()
+    end_date = models.DateField()
+    patient = models.ForeignKey(PatientDetails, on_delete=models.PROTECT, related_name="problem_patient")
+    doctor = models.ForeignKey(DoctorDetails, on_delete=models.PROTECT)
+
+
+class SocialHistory:
+    SMOKE_STATUS = [
+        ("1", "Never Smoked"),
+        ("2", "Current Smoker"),
+        ("3", "Former Smoker")
+    ]
+    DRINK_STATUS = [
+        ("1", "Current drinker"),
+        ("2", "Former drinker"),
+        ("3", "Lifetime Non-drinker")
+    ]
+    tobacco = models.CharField(choices=SMOKE_STATUS, default="Never Smoked")
+    alcohol = models.CharField(choices=DRINK_STATUS, default="Current Drinker")
+    patient = models.ForeignKey(PatientDetails, on_delete=models.PROTECT, related_name='patient_smoker')
+
+
+
