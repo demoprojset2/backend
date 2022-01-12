fa905478a919c2887770a913d3ba9ce306b39eec
dnirmal1 <aishpatil19999@gmail.com>
2022-01-12 20:11:42 +0800

    created serializer

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,9 @@
+
 import uuid
 from django.db import models
 from django.core.validators import MinLengthValidator
 from phonenumber_field.modelfields import PhoneNumberField
+
 
 
 # Create your models here.
