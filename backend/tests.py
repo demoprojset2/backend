7ebf67c7ddcc5ef707bb797218932eb6a4cd69df
sonukumar-a11 <sharmasonu04359491@gmail.com>
2022-01-23 18:53:03 +0530

    Activate ActionWork Flow

--- backend/tests.py
+++ backend/tests.py
@@ -44,7 +44,7 @@ class PatientTestCase(TestCase):
     def test_field_Medication_creation(self):
         self.user_as_doctor = User.objects.create(email='sharmasonu04359491@gmail.com')
         self.doctor = Profile.objects.create(user=self.user_as_doctor, speciality="Ortho", gender="M")
-        patient = PatientDetails.objects.create(name="Sonu", email_id="sonuraj1926@gmail.com", gender="M",
+        PatientDetails.objects.create(name="Sonu", email_id="sonuraj1926@gmail.com", gender="M",
                                                 address="Sector-49", dob="1999-09-06",
                                                 phone_number="+918527879473", doctor=self.doctor)
         medication = Medication(medication_name="Dolo", medication_manufacturer="Apex", expire="2021-01-01", amount=104)
